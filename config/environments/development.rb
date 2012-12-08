93ba7c809449276ef415af50ccf597515de1565c
yeaz <yeaz@stanford.edu>
2012-12-08 09:01:16 +0900

    removed tls setting in smtp setting for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Kmlaalumni::Application.configure do
   
   # Mailer settings
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port:  "587",
@@ -44,6 +46,5 @@ Kmlaalumni::Application.configure do
     authentication: :plain,
     user_name: "yeaz90@gmail.com",
     password: "yeaz11235813",
-    tls: true
   }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Kmlaalumni::Application.configure do
   
   # Mailer settings
   config.action_mailer.default_url_options = { host: 'kmlaalumni.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port:  "587",
@@ -74,6 +76,5 @@ Kmlaalumni::Application.configure do
     authentication: :plain,
     user_name: "yeaz90@gmail.com",
     password: "yeaz11235813",
-    tls: true
   }
 end
