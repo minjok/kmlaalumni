f0ab513b401206f124d0a91e73951934668fe7ec
yeaz <yeaz@stanford.edu>
2013-01-16 13:09:56 +0900

    fixed newline display in posting by overwriting simple_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,14 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+  
+  def simple_format(text)
+    text = '' if text.nil?
+    text = text.dup
+    text = sanitize(text)
+    text = text.to_str
+    text.gsub!(/\r\n/, "<br>")                    # \r\n and \r -> \n
+    text.html_safe
+  end
  
 end
