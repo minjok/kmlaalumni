5ccc3dd9feff0234a2d310484a6870b00db9554e
yeaz <yeaz@stanford.edu>
2013-01-27 15:08:15 +0900

    added admin to users migration

---
+++ db/migrate/20130127001647_add_admin_to_user.rb
@@ -0,0 +1,12 @@
+class AddAdminToUser < ActiveRecord::Migration
+  def up
+    change_table :users do |t|
+      t.boolean :is_admin, :default => false
+    end
+    User.update_all ["is_admin = ?", false]
+  end
+ 
+  def down
+    remove_column :users, :is_admin
+  end
+end
