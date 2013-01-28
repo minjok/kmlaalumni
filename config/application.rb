3f9388bbdb865e32d2f27f0d91855185284f9449
yeaz <yeaz@stanford.edu>
2013-01-28 16:45:34 +0900

    enabled assets sync with s3 bucket

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,7 @@ module Kmlaalumni
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.digest = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
