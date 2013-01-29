77bdd56ca0baba96720a175dd9ede5f2d8a75ac2
yeaz <yeaz@stanford.edu>
2013-01-29 16:08:22 +0900

    added kmla.ico to precompile assets list in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Kmlaalumni::Application.configure do
   config.action_controller.asset_host = "https://s3-ap-northeast-1.amazonaws.com/kmlaalumni-assets"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( kmla.ico )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
