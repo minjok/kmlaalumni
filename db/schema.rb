c18987af35b975b50597b8a0a3f9aa0ede0465c0
PJ <lastspurt17@gmail.com>
2013-03-07 21:02:41 +0900

    CareerNote Update 7:HtmlEditor, ProfilePage Change

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130303120955) do
     t.integer  "user_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "careernote_id"
     t.integer  "commentable_id"
     t.string   "commentable_type"
   end
