43da3628524e1cdf1a980e4220a83e74477bdd7c
PJ <lastspurt17@gmail.com>
2013-03-15 21:27:20 +0900

    CareerNote Feed 2

--- app/helpers/careernotes_helper.rb
+++ app/helpers/careernotes_helper.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 module CareernotesHelper
-
+  require 'action_view'
+  
   def link_to_commentable_modification(commentable)
     if commentable.is_a? Posting
       link_to '삭제', posting_url(commentable), method: :delete, remote: true
@@ -11,4 +12,12 @@ module CareernotesHelper
     end
   end
   
+  # Method: strip_html(text)
+  # --------------------------------------------
+  # Removes all html tags from text
+  def strip_html(text)
+    unless text.nil?
+      strip_tags(text)
+    end
+  end
 end
